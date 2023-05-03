4c245f2ab93f193ff63a01cf972b1f328cedb34c
ramkumar <ramkumarkandasamy@gmail.com>
2023-05-03 03:06:11 +0000

    why it's occur how to solve this error

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UserProfile from './components/UserProfile/index'
+
+const userDetails = {
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Ramkumar',
+    role: 'SoftwareDeveloper'
+}
+
+const App = () => <UserProfile />
+
+export default App
